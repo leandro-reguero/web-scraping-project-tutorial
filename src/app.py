b6d932d14d5d5010abc4a950407b8a0cba5786a8
leandro-reguero <lreguerolazaro@gmail.com>
2024-06-27 17:47:58 +0200

    VAMOSS porfin he scrapeado las fechas y valores bien!!

--- src/app.py
+++ src/app.py
@@ -6,18 +6,28 @@ import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
 import re
+from numpy import random
+import numpy as np
+
 
 
 # Defining the GET function to start scraping
 def get_scraped():
     global url
     url = 'https://ycharts.com/companies/TSLA/revenues'
-    # User-Agent 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
-               }
+
+    # User-Agent rotation
+    user_agent_list = [ 
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36', 
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15', 
+    ]
+    for i in range(1, 4):
+        user_agent = random.choice(user_agent_list)
+
+    headers = {'User-Agent': user_agent}
     
-    # GET Request with User-Agent 
+    # GET Request with User-Agent choice
     response = requests.get(url, headers=headers)
     time.sleep(9)
     if response.status_code == 200:
@@ -27,5 +37,10 @@ def get_scraped():
     else:
         print(f"There was an error with code: {response.status_code}")
 
-
 get_scraped()
+
+# table data (td) sin class = date
+# table data (td) class text-right = value
+
+# div row, div col-6, table table
+
