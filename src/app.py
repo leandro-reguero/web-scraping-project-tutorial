ece0196c763b0b8671b63e87c03876089006072f
leandro-reguero <lreguerolazaro@gmail.com>
2024-06-27 18:02:52 +0200

    dataframe hecho y limpio

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import re
 from numpy import random
 import numpy as np
+import pandas as pd
 
 
 
@@ -32,15 +33,37 @@ def get_scraped():
     time.sleep(9)
     if response.status_code == 200:
         html_data = response.text
-        print(html_data)
+        global soup
+        soup = BeautifulSoup(response.content, 'html.parser')
 
     else:
         print(f"There was an error with code: {response.status_code}")
 
 get_scraped()
 
-# table data (td) sin class = date
-# table data (td) class text-right = value
+v_list = []
+d_list = []
+tables = soup.find_all('table', class_='table')
 
-# div row, div col-6, table table
+
+dates = tables[0].find_all('td', class_=None) + tables[1].find_all('td', class_=None)
+values = tables[0].find_all('td', class_='text-right') + tables[1].find_all('td', class_='text-right')
+
+
+for value in values:
+    v_list.append(value.text.strip())
+
+for date in dates:
+    d_list.append(date.text.strip())
+
+# Creando el DataFrame
+dicto = {'Date': d_list, 'Value': v_list}
+df = pd.DataFrame(dicto)
+
+#Procesando y limpiando el DataFrame
+df['Date'] = pd.to_datetime(df['Date'])
+in_b = {'M': '*0.001', 'B': ''}
+df['Value'] = df['Value'].replace(in_b, regex=True).map(pd.eval).round(3)
+
+print(df)
 
